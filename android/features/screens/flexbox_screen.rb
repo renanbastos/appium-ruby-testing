f5e62b8ef438b77917b3c02331c86eeeb100b9cf
Renan Bastos <renanbastos86@gmail.com>
2019-10-11 17:27:37 -0300

    remove linhas comentadas

--- android/features/screens/flexbox_screen.rb
+++ android/features/screens/flexbox_screen.rb
@@ -6,7 +6,6 @@ class MessagesScreen
     def initialize
       @select = 'com.android.mms:id/recipients_editor_to'
       @mensagem = 'com.android.mms:id/editor_body'
-      # @usuario = 'com.google.android.apps.messaging:id/contact_picker_create_group'
       @envia = 'com.android.mms:id/send_button'
       @chat = 'com.android.mms:id/floating_action_button'
 
@@ -17,7 +16,6 @@ class MessagesScreen
         when "chat" then find_element(:id, @chat).click
         when "select" then find_element(:id, @select).click
         when "la" then find_element(:uiautomator, 'new UiSelector().text("Insira destinatários")').send_keys("240").press_keycode(66).manage.timeouts.implicit_wait = 5
-        # when "usuario" then find_element(:id, @usuario).click
         when "sms" then find_element(:id, @mensagem).send_keys("tech")
         when "send" then find_element(:id, @envia).click
       end
