4569d34c93c6b40654d73fbaab244df0f2520375
Renan Bastos <renanbastos86@gmail.com>
2019-10-11 17:22:09 -0300

    add modificacao de find_element para usar uiautomator, press_keycode e timeout

--- android/features/screens/flexbox_screen.rb
+++ android/features/screens/flexbox_screen.rb
@@ -7,7 +7,6 @@ class MessagesScreen
       @select = 'com.google.android.apps.messaging:id/recipient_text_view'
       @mensagem = 'com.google.android.apps.messaging:id/compose_message_text'
       @usuario = 'com.google.android.apps.messaging:id/contact_picker_create_group'
-      @tela = 'com.google.android.apps.messaging:id/action_bar_root'
       @envia = 'com.google.android.apps.messaging:id/send_message_button_icon'
       @chat = 'com.google.android.apps.messaging:id/start_new_conversation_button'
 
@@ -17,7 +16,7 @@ class MessagesScreen
       case elemento
         when "chat" then find_element(:id, @chat).click
         when "select" then find_element(:id, @select).click
-        when "la" then find_element(:accessibility_id, @select).send_keys("222222")
+        when "la" then find_element(:uiautomator, 'new UiSelector().text("Digite um nome, número de telefone ou endereço de e-mail")').send_keys("240").press_keycode(66).manage.timeouts.implicit_wait = 5
         when "usuario" then find_element(:id, @usuario).click
         when "sms" then find_element(:id, @mensagem).send_keys("mensagem de texto")
         when "send" then find_element(:id, @envia).click
